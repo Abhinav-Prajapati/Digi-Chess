9b47f958a346d34393f326040f440ac488cd952d
Titan <76146412+Abhinav-Prajapati@users.noreply.github.com>
2021-12-07 17:21:29 +0530

    working fine

--- digiBoard/pyboard.py
+++ digiBoard/pyboard.py
@@ -1,41 +1,29 @@
 import numpy as np
+import chess
 
 
-old_matrix = np.array([[ 1, 1, 1, 1 ], 
-                       [ 0, 0, 0, 0 ], 
-                       [ 0, 0, 0, 0 ],  
-                       [ 1, 1, 1, 1 ]])
+board=chess.Board()
 
-new_matrix = np.array([[ 1, 1, 1, 1 ], # 4
-                       [ 0, 0, 0, 0 ], # 3
-                       [ 0, 0, 0, 0 ], # 2
-                       [ 1, 1, 1, 1 ]])# 1
-                    #    a  b  c  d
+old_matrix = np.array([[ 1, 1, 1, 1, 1, 1, 1, 1], 
+                       [ 1, 1, 1, 1, 1, 1, 1, 1], 
+                       [ 0, 0, 0, 0, 0, 0, 0, 0], 
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 1, 1, 1, 1, 1, 1, 1, 1],
+                       [ 1, 1, 1, 1, 1, 1, 1, 1]])
+
+new_matrix = np.array([[ 1, 1, 1, 1, 1, 1, 1, 1], 
+                       [ 1, 1, 1, 1, 1, 1, 1, 1], 
+                       [ 0, 0, 0, 0, 0, 0, 0, 0], 
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 0, 0, 0, 0, 0, 0, 0, 0],
+                       [ 1, 1, 1, 1, 1, 1, 1, 1],
+                       [ 1, 1, 1, 1, 1, 1, 1, 1]])
 
 
-sign=[]
-move=[]
-
-
-def get_sign(matrix):
-    #This function return True if matrix contain 1.  (place)
-    #and False if it contain -1.   (pick)
-    for i in  matrix:
-
-        if np.sum(i)==1:
-            return True
-
-        elif np.sum(i)==-1:
-            return False
-            
-def matrix_to_notation(matrix):
-    '''
-    this function return chess notation based of position on 1 or -1 in a 2D array
-    '''
-    #column is for alphabet
-    #row is for number
-    
-    '''
+'''
 
     8 [1,1,1,1,1,1,1,1],
     7 [1,1,1,1,1,1,1,1],
@@ -47,9 +35,30 @@ def matrix_to_notation(matrix):
     1 [1,1,1,1,1,1,1,1]
        a b c d e f g h 
 
+'''
+move=[]
+sign=[]
+
+
+
+           
+def unit_matrix_to_notation(matrix):
     '''
-    a=np.where(matrix !=0) # IF NUMBER IS 1 OR -1
-    
+    this function return chess notation based of position on 1 or -1 in a 2D array
+    '''
+    def get_sign(matrix_):
+        #This function return True if matrix contain 1.  (place)
+        #and False if it contain -1.   (pick)
+        for i in  matrix_:
+
+            if np.sum(i)==1:
+                return "place"
+
+            elif np.sum(i)==-1:
+                return "pick"
+
+    a=np.where(matrix !=0) # find array where 1 OR -1 is present.
+    # a=[row,column] this is position of 1 OR -1
     rows=a[0]    # find the row where 1 or -1 is present
     column=a[1]  # find the column where 1 or -1 is present
        
@@ -58,9 +67,12 @@ def matrix_to_notation(matrix):
 
     alp=chr(97+column[0])   # convert number to alphabet ie. 1->a , 2->b , 3->c and so on      
     notation=alp+str(num[0])  # alphabet and number
-
     #print(notation)
-    return notation
+
+    sign_=get_sign(matrix)
+
+    return [notation,sign_] # output formate [['d5', 'place']]
+
 
 def is_sufficent_move(a):
     # Check wether there is enough move or not
@@ -71,25 +83,6 @@ def is_sufficent_move(a):
     else:
         return False
 
-def update_new_matrix():
-    # little bit of twiking in needed tho it work as intended
-
-    '''
-    this function take input a single row 
-    ie."1001" first number represent row number 
-    "001" represent valur of that row in matrix  
-    new_matrit[1]=[0,0,1]
-    '''
-    a= input("Enter matrix")
-    a.split()
-    
-    a= list(map(int, a))
-    rows,columns=new_matrix.shape
-    list_index=rows - a[0]
-    new_matrix[list_index]=a[1:]
-    #print(list_index)
-
-
 ##################################################################
 ################### DIFFERENT TYPE OF MOVES ######################
 ##################################################################
@@ -98,16 +91,22 @@ def update_new_matrix():
 def normal_move():
     from_=move[0]
     to=move[1]
-    sign.clear()
+    
     move.clear()
+    sign.clear()
 
     return from_+to
 #.................................................................
-
+   
 
 #.....................Capture move................................
 def capture_move(): 
+
+
+
+
     #refer notes to understand this
+    # this function work as intended but it is complete mess
     if move[1]==move[2]:
         from_=move[1]
         to=move[0]
@@ -116,8 +115,10 @@ def capture_move():
         to= move[0]
     move.clear()
     sign.clear()
+  
 
-    return from_ + to
+    return  to + from_ # this 
+    
 #.................................................................
 
 
@@ -143,39 +144,86 @@ def Pawn_Promotion():
 
 
 
-def main():
-    while True:
-
-        old_matrix=np.copy(new_matrix)
-        update_new_matrix()
-
-        #print(new_matrix)
-        #print(old_matrix)
-
-        resultant_matrix=new_matrix-old_matrix
 
 
-        #....................................................
-        Sign=get_sign(resultant_matrix)
-        sign.append(Sign)
-        Move=matrix_to_notation(resultant_matrix)
-        move.append(Move)
-        #....................................................
+def update_matrix(row_num,new_list):
+
+    old_matrix = np.copy(new_matrix)
+
+    new_matrix[row_num]=new_list    # UPDATE THE NEW MATRIX WITH NEW ARRAY
+   
+    resultant_matrix=new_matrix-old_matrix
+    #
+    # print(resultant_matrix)
+
+    ab=unit_matrix_to_notation(resultant_matrix)
+    
+    move.append(ab[0])
+    sign.append(ab[1])
+#.............................................................................
+    #print(move)
+
+    if len(move)==2 and (move[0]!= move[1]) and sign[0]=="pick" and  sign[1]=="place":
+        return ["Normal move ",normal_move()]
+
+    elif len(move)==3 and (move[2]==move[1] or move[2]==move[0]) :
+        return ["Capture move ",capture_move()]
 
 
-        #....................................................
-        #print(f"Enough move is :{is_sufficent_move(sign)}")
-        print(f'notation :{move}')
-        print(f'Sign :{sign}')
-        #....................................................
-        
-        if is_sufficent_move(sign)==True:
 
-            if len(move)==2:
-                print(normal_move())
 
-            elif len(move)==3:
-                print(capture_move()) 
-        
-if __name__=="__main__":
-    main() # for testing purpose only
+#print(unit_matrix_to_notation((new_matrix-old_matrix)))
+
+if __name__=="__main__": # for module testing ouly
+
+
+    v=update_matrix(1,[0,1,1,1,1,1,1,1])
+    v=update_matrix(1,[0,0,1,1,1,1,1,1])
+    v=update_matrix(1,[0,1,1,1,1,1,1,1])
+    print(v)
+    v=update_matrix(1,[0,0,1,1,1,1,1,1])
+    v=update_matrix(1,[1,0,1,1,1,1,1,1])
+    print(sign)
+    print(v)
+    print(move)
+
+
+
+
+
+
+
+'''
+def validate_input(row_num,new_list):
+       # -> This function validates the input give to it
+        #-> row_number must
+        #  be in 1,2,3,4,5,6,7,8 (or)
+        #-> length of new list must be 8
+       # -> and difference of  sum of input_list and new_matrix[row_num] must be 1 of -1
+    
+    #................................................
+    if row_num in range(1,9): # if row number is 1,2,3,4,5,6,7,8 (or)
+        out =True
+    else: 
+        out=False
+        print(f'{row_num} Is Invalid Row number')
+    
+     
+    a=sum(new_list)-np.sum(new_matrix[row_num-1])
+    #................................................
+    if out==True:
+    
+        if len(new_list)==8 and abs(a)==1:
+            out2=True
+        else:
+            out2= False
+            print(f'{new_list} Is a Invalid list ')
+    #................................................
+    if out and out2:
+        return True
+    else:
+        return False
+print(validate_input(9,[0,0,0,0,0,0,0,0]))       
+'''
+
+
